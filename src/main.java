db1f69dd63ebdc0e3cad051ad261028a35605b36
andreygork <agorkaev99@gmail.com>
2022-07-11 19:28:35 +0300

    correct

--- src/main.java
+++ src/main.java
@@ -2,8 +2,8 @@ public class main {
     public static void main (String[] args) {
         BmiService service = new BmiService();
         double weight = 76.5;
-        double heiht = 1.80;
-        double index = service.calculate(weight, heiht);
+        double height = 1.80;
+        double index = service.calculate(weight, height);
         System.out.println(index);
     }
 }
